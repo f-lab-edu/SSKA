52a9154159add2cee86ee6284b936318c0cbec84
Taewoongjung <aipooh8882@naver.com>
2022-11-25 13:30:42 +0900

    first commit

---
+++ src/main/java/com/skka/SkkaApplication.java
@@ -0,0 +1,13 @@
+package com.skka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skka/SkkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
