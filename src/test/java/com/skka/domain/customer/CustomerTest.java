03d671a81440861ec0686322bc54e6b1bf0e7cb0
Taewoongjung <aipooh8882@naver.com>
2022-12-02 23:00:49 +0900

    customer test 추가

--- src/test/java/com/skka/domain/customer/CustomerTest.java
+++ src/test/java/com/skka/domain/customer/CustomerTest.java
@@ -1,7 +1,52 @@
 package com.skka.domain.customer;
 
+import static com.skka.customer.CustomerFixture.CUSTOMER;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
+import com.skka.domain.customer.error.InvalidCustomerException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Customer 는 ")
 class CustomerTest {
 
+    @DisplayName("생성된다")
+    @Test
+    void test1() {
+        assertDoesNotThrow(() -> new Customer (
+            CUSTOMER.getId(),
+            CUSTOMER.getName(),
+            CUSTOMER.getEmail(),
+            CUSTOMER.getPassword(),
+            CUSTOMER.getTel()
+        ));
+    }
+
+    @DisplayName("필수값 이름이 없을 때 생성 되지 않는다")
+    @Test
+    void test2() {
+        assertThatThrownBy(() -> new Customer (
+            CUSTOMER.getId(), null, CUSTOMER.getEmail(), CUSTOMER.getPassword(), CUSTOMER.getTel()))
+            .isInstanceOf(InvalidCustomerException.class)
+            .hasMessage("올바르지 않은 이름입니다.");
+    }
+
+    @DisplayName("필수값 이메일이 없을 때 생성 되지 않는다")
+    @Test
+    void test3() {
+        assertThatThrownBy(() -> new Customer (
+            CUSTOMER.getId(), CUSTOMER.getName(), null, CUSTOMER.getPassword(), CUSTOMER.getTel()))
+            .isInstanceOf(InvalidCustomerException.class)
+            .hasMessage("올바르지 않은 이메일입니다.");
+    }
+
+    @DisplayName("필수값 전화번호가 없을 때 생성 되지 않는다")
+    @Test
+    void test4() {
+        assertThatThrownBy(() -> new Customer (
+            CUSTOMER.getId(), CUSTOMER.getName(), CUSTOMER.getEmail(), CUSTOMER.getPassword(), null))
+            .isInstanceOf(InvalidCustomerException.class)
+            .hasMessage("올바르지 않은 전화번호입니다.");
+    }
 }
