8a37814dffbbc077ab92e858d413ada37c887f05
Taewoongjung <aipooh8882@naver.com>
2022-12-04 23:03:30 +0900

    baseEntity 객체 생성시 시간 할당

--- src/main/java/com/skka/adaptor/common/domain/BaseEntity.java
+++ src/main/java/com/skka/adaptor/common/domain/BaseEntity.java
@@ -5,26 +5,24 @@ import javax.persistence.Column;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import lombok.Getter;
-import lombok.Setter;
-import org.hibernate.annotations.ColumnDefault;
-import org.hibernate.annotations.Generated;
-import org.hibernate.annotations.GenerationTime;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
+@RequiredArgsConstructor
+@ToString
 @Getter
-public class BaseEntity {
-
-    LocalDateTime now = LocalDateTime.now();
+public abstract class BaseEntity {
 
     @Column(nullable = false, updatable = false, name = "created_at")
     @CreatedDate
-    private LocalDateTime createdAt = now;
+    private final LocalDateTime createdAt;
 
     @Column(nullable = false, name = "last_modified")
     @LastModifiedDate
-    private LocalDateTime updatedAt = now;
+    private final LocalDateTime lastModified;
 }
--- src/main/java/com/skka/domain/customer/Customer.java
+++ src/main/java/com/skka/domain/customer/Customer.java
@@ -7,6 +7,7 @@ import static com.skka.adaptor.common.exception.ErrorType.INVALID_CUSTOMER_TEL;
 import com.skka.adaptor.common.domain.BaseEntity;
 import com.skka.adaptor.common.exception.ErrorType;
 import com.skka.domain.customer.error.InvalidCustomerException;
+import java.time.LocalDateTime;
 import java.util.function.Predicate;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -14,15 +15,12 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import lombok.AccessLevel;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Entity
 @Table(name = "customer")
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@ToString
+@ToString(callSuper = true)
 @Getter
 public class Customer extends BaseEntity {
 
@@ -42,6 +40,10 @@ public class Customer extends BaseEntity {
     @Column(name = "tel", length = 20, nullable = false)
     private String tel;
 
+    protected Customer() {
+        super(LocalDateTime.now(), LocalDateTime.now());
+    }
+
     public Customer(
         final long id,
         final String name,
@@ -49,6 +51,8 @@ public class Customer extends BaseEntity {
         final String password,
         final String tel
     ) {
+        super(LocalDateTime.now(), LocalDateTime.now());
+
         require(o -> name == null, name, INVALID_CUSTOMER_NAME);
         require(o -> email == null, email, INVALID_CUSTOMER_EMAIL);
         require(o -> tel == null, tel, INVALID_CUSTOMER_TEL);
