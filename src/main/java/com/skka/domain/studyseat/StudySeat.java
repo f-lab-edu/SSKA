cf1029d6a7012184e291b0e82a7a926df56bca64
Taewoongjung <aipooh8882@naver.com>
2022-12-16 21:44:57 +0900

    Schedule 추가 하므로 StudySeat에 Customer 매핑 취소

--- src/main/java/com/skka/domain/studyseat/StudySeat.java
+++ src/main/java/com/skka/domain/studyseat/StudySeat.java
@@ -13,8 +13,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -37,10 +35,6 @@ public class StudySeat {
 
     @Embedded
     private StartEndTime startEndTime;
-
-    @OneToOne
-    @JoinColumn(name = "customer_id")
-    private Customer customer;
 
     public StudySeat(
         final long id,
