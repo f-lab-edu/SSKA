a399191138b6790e9553c97a8801d5cd80532aa0
Taewoongjung <aipooh8882@naver.com>
2022-12-23 20:13:11 +0900

    schedule table 수정

--- src/main/resources/db/migration/V5__schedule.sql
+++ src/main/resources/db/migration/V5__schedule.sql
@@ -3,8 +3,8 @@ CREATE TABLE `schedule`
     `id`            BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
     `customer_id`   BIGINT(20) UNSIGNED     NULL                  COMMENT '고객 id',
     `study_seat_id` BIGINT(20) UNSIGNED     NULL                  COMMENT '좌석 id',
-    `started_time`  DATETIME(6)         NOT NULL                  COMMENT '시작 시긱',
-    `end_time`      DATETIME(6)         NOT NULL                  COMMENT '끝나는 시각',
+    `started_time`  DATETIME(6)         NOT NULL                  COMMENT '시작 시간',
+    `end_time`      DATETIME(6)         NOT NULL                  COMMENT '끝나는 시간',
     PRIMARY KEY (`id`),
     FOREIGN KEY (customer_id) REFERENCES customer (id),
     FOREIGN KEY (study_seat_id) REFERENCES study_seat (id)
