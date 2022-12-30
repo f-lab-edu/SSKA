7ba823699cb53da01d2de1d9c6f411e40e725e1e
Taewoongjung <aipooh8882@naver.com>
2022-12-30 16:01:06 +0900

    ScheduleRepository 추가

---
+++ src/main/java/com/skka/domain/schedule/repository/ScheduleRepository.java
@@ -0,0 +1,14 @@
+package com.skka.domain.schedule.repository;
+
+import com.skka.domain.schedule.Schedule;
+import java.time.LocalDateTime;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
+
+}
