0a8170da4fd98e92c4f3edf7703d36f2af9c9351
Taewoongjung <aipooh8882@naver.com>
2022-12-30 16:00:36 +0900

    CustomerRepository 추가

---
+++ src/main/java/com/skka/domain/customer/repository/CustomerRepository.java
@@ -0,0 +1,10 @@
+package com.skka.domain.customer.repository;
+
+import com.skka.domain.customer.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+}
