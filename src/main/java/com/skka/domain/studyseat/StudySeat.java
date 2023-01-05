1bef63462c3563d9574c5c6a72d64c55617c167d
Taewoongjung <aipooh8882@naver.com>
2023-01-06 02:03:48 +0900

    버그 수정

--- src/main/java/com/skka/domain/studyseat/StudySeat.java
+++ src/main/java/com/skka/domain/studyseat/StudySeat.java
@@ -9,6 +9,7 @@ import com.skka.domain.schedule.Schedule;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
