ba348f6d24dd6f8100afbb4c7fd0c5a47e1a4f54
Taewoongjung <aipooh8882@naver.com>
2023-01-11 05:43:05 +0900

    base infrastructure

---
+++ src/main/java/com/skka/application/studyseat/StudySeatService.java
@@ -0,0 +1,10 @@
+package com.skka.application.studyseat;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class StudySeatService {
+
+}
