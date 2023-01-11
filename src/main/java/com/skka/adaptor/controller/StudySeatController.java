3140ca5f20c1f4f4e6f630e2c965667280de29ce
Taewoongjung <aipooh8882@naver.com>
2023-01-11 11:38:07 +0900

    controller 추가

---
+++ src/main/java/com/skka/adaptor/controller/StudySeatController.java
@@ -0,0 +1,36 @@
+package com.skka.adaptor.controller;
+
+import com.skka.application.studyseat.StudySeatService;
+import com.skka.application.studyseat.dto.ReserveSeatRequest;
+import com.skka.application.studyseat.response.CommandReserveSeatResponse;
+import com.skka.application.studyseat.webrequest.CommandReserveSeatWebRequestV1;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class StudySeatController {
+
+    private final StudySeatService customerService;
+
+    @PostMapping(value = "/seat/{studySeatId}")
+    public ResponseEntity<CommandReserveSeatResponse> reserveSeat(
+        final CommandReserveSeatWebRequestV1 command,
+        @PathVariable final long studySeatId
+    ) {
+        ReserveSeatRequest commandService = new ReserveSeatRequest(
+            command.getCustomerId(),
+            command.getStartedTime(),
+            command.getEndTime()
+        );
+
+        return ResponseEntity.status(HttpStatus.OK).body(customerService.reserveSeat(
+            commandService,
+            studySeatId
+        ));
+    }
+}
