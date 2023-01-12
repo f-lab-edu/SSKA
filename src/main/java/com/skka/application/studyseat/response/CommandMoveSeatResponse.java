84fec25a8f944945ecefb63ef6dad192cc3bb0c1
Taewoongjung <aipooh8882@naver.com>
2023-01-12 22:27:54 +0900

    dto, response, webrequest 추가

---
+++ src/main/java/com/skka/application/studyseat/response/CommandMoveSeatResponse.java
@@ -0,0 +1,11 @@
+package com.skka.application.studyseat.response;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class CommandMoveSeatResponse {
+    private final String message;
+    private final long movedSeatId;
+}
