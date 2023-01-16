69e640b0c6f2a490e859e401abb957a27cc7ac96
Taewoongjung <aipooh8882@naver.com>
2023-01-16 16:28:18 +0900

    commandMoveResponse 수정

--- src/main/java/com/skka/application/studyseat/response/CommandMoveSeatResponse.java
+++ src/main/java/com/skka/application/studyseat/response/CommandMoveSeatResponse.java
@@ -1,11 +1,45 @@
 package com.skka.application.studyseat.response;
 
+import java.time.LocalDateTime;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
 public class CommandMoveSeatResponse {
-    private final String message;
-    private final long movedSeatId;
+    private String message;
+    private Long movedSeatId;
+    private LocalDateTime changingStartedTime;
+    private LocalDateTime changingEndTime;
+
+    public CommandMoveSeatResponse(
+        final String message,
+        final Long movedSeatId,
+        final LocalDateTime changingStartedTime,
+        final LocalDateTime changingEndTime
+    ) {
+        this.message = message;
+        this.movedSeatId = movedSeatId;
+        this.changingStartedTime = changingStartedTime;
+        this.changingEndTime = changingEndTime;
+    }
+
+    public static CommandMoveSeatResponse of(
+        final String message,
+        final Long movedSeatId
+    ) {
+        return new CommandMoveSeatResponse(
+            message, movedSeatId,
+            null, null
+        );
+    }
+
+    public static CommandMoveSeatResponse of(
+        final String message,
+        final LocalDateTime changingStartedTime,
+        final LocalDateTime changingEndTime
+    ) {
+        return new CommandMoveSeatResponse(
+            message, null,
+            changingStartedTime, changingEndTime
+        );
+    }
 }
