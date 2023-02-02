310893a13e0d255c79853518a23036b695042d4b
Taewoongjung <aipooh8882@naver.com>
2023-02-02 22:51:46 +0900

    ga changed

--- src/main/java/com/skka/application/studyseat/StudySeatService.java
+++ src/main/java/com/skka/application/studyseat/StudySeatService.java
@@ -19,6 +19,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class StudySeatService {
 
