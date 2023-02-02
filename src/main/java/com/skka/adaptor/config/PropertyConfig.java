3ef3831b2df2ac6243b6ca414f85abc3d5db8be3
Taewoongjung <aipooh8882@naver.com>
2023-02-03 03:41:51 +0900

    refactor

--- src/main/java/com/skka/adaptor/config/PropertyConfig.java
+++ src/main/java/com/skka/adaptor/config/PropertyConfig.java
@@ -1,4 +1,4 @@
-package com.skka.adaptor;
+package com.skka.adaptor.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
